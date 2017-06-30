a28437bacbaea9b332eba14caaec767433a1039b
Todd Eisenberger <teisenbe@google.com>
2017-06-30 16:46:59 -0700

    [ddk] Begin routing BTIs through
    Change-Id: Iec6a70adc5f7fe40bf709a598255f28c60817da2

--- system/dev/bus/pci/protocol.c
+++ system/dev/bus/pci/protocol.c
@@ -15,6 +15,20 @@ static mx_status_t pci_enable_bus_master(void* ctx, bool enable) {
     return mx_pci_enable_bus_master(device->handle, enable);
 }
 
+static mx_status_t pci_get_bti(void* ctx, mx_handle_t* out_handle) {
+    kpci_device_t* device = ctx;
+
+    for (size_t i = 0; i < countof(device->props); ++i) {
+        if (device->props[i].id == BIND_PCI_BDF_ADDR) {
+            const uint32_t bdf = device->props[i].id;
+            const mx_handle_t iommu_handle = MX_HANDLE_INVALID;
+            return mx_bti_create(iommu_handle, bdf, out_handle);
+        }
+    }
+
+    return MX_ERR_NOT_SUPPORTED;
+}
+
 static mx_status_t pci_enable_pio(void* ctx, bool enable) {
     kpci_device_t* device = ctx;
     return mx_pci_enable_pio(device->handle, enable);
@@ -186,6 +200,7 @@ static mx_status_t pci_get_device_info(void* ctx, mx_pcie_device_info_t* out_inf
 
 static pci_protocol_ops_t _pci_protocol = {
     .enable_bus_master = pci_enable_bus_master,
+    .get_bti = pci_get_bti,
     .enable_pio = pci_enable_pio,
     .reset_device = pci_reset_device,
     .map_resource = pci_map_resource,
